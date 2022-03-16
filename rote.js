de237736174afc7e10626b343a4ade2e37d58b46
Michael Law <michaellaw@gmx.com>
2022-03-16 13:31:29 -0400

    update

---
+++ rote.js
@@ -0,0 +1,17 @@
+const greet = () => {
+  console.log('Hello world')
+}
+
+greet()
+
+const square = (num) => {
+  return num * num
+}
+
+square(25)
+
+const greetByName = (str) => {
+  console.log('Hello', str)
+}
+
+greetByName('michael')
