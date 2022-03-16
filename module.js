442f4d27d76b9371e52884dfeb6d6344b5953cfa
Michael Law <michaellaw@gmx.com>
2022-03-16 10:18:11 -0400

    added function to button

--- module.js
+++ module.js
@@ -9,10 +9,11 @@
 
 */
 
-let myName = prompt('Enter your name')
-let month = prompt('What month were you born?')
-let food = prompt('What is your favorite food?')
-
-console.log('My name is', myName)
-console.log('I was born in', month)
-console.log('My favorite food is', food)
+function quiz() {
+  let myName = prompt('Enter your name')
+  let month = prompt('What month were you born?')
+  let food = prompt('What is your favorite food?')
+  console.log('My name is', myName)
+  console.log('I was born in', month)
+  console.log('My favorite food is', food)
+}
